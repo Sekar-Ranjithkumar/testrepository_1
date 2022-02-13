2b78aad1b30740d899c58fcba58345224c008362
Sekar-Ranjithkumar <99594611+Sekar-Ranjithkumar@users.noreply.github.com>
2022-02-13 17:40:42 +0530

    Create First_Program.py
    Adding new code

---
+++ First_Program.py
@@ -0,0 +1,9 @@
+# Addition of two numbers using function
+def sum(a,b):
+  c = a + b
+  print("Sum of two numbers : ",c)
+  
+a = int(input("Enter first number : "))
+b = int(input("Enter second number : "))
+
+sum(a,b)
